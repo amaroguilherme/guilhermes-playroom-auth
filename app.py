bf01e25881c2e974170c48624b06aee1a03d3990
Guilherme Alves <guilherme.amaro@ibm.com>
2021-03-31 15:30:48 -0300

    solves CORS issues

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+from flask_cors import CORS
 
 from security import authenticate, identity
 from user import UserRegister
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 app.secret_key = 'gui'
 api = Api(app)
 
