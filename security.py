9ced891b8851951c7d9d4740522b1ebae82b4cb7
Guilherme Alves <guilherme.amaro@ibm.com>
2021-03-31 13:45:12 -0300

    changes auth to username

--- security.py
+++ security.py
@@ -1,8 +1,8 @@
 from user import User
 from werkzeug.security import safe_str_cmp
 
-def authenticate(email, password):
-  user = User.find_by_email(email)
+def authenticate(username, password):
+  user = User.find_by_username(username)
   if user and safe_str_cmp(user.password, password):
     return user
 
